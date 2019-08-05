a68fb0207715a7a0d32bcf736590f59e8ee1f5cc
Tim Western <veretax@gmail.com>
2019-08-05 19:06:10 -0400

    Setup Database
    Create User table
    Create sp User Lookup stored procedure

---
+++ WebApiManager.Data/dbo/Stored Procedures/sp_User_Lookup.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[sp_UserLookup]
+	@Id nvarchar(128)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT [Id], [FirstName], [LastName], [Email], [CreatedDate] 
+	FROM [dbo].[User]
+	WHERE [dbo].[User].Id = @Id
+END

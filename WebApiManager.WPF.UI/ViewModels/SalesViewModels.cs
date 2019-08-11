03921ee055c59d7a46aceed71191e2974cac1905
Tim Western <veretax@gmail.com>
2019-08-11 12:36:20 -0400

    Create Sales View Page and model

---
+++ WebApiManager.WPF.UI/ViewModels/SalesViewModels.cs
@@ -0,0 +1,148 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiManager.WPF.UI.ViewModels
+{
+    public class SalesViewModels : Screen
+    {
+        private BindingList<string> _products;
+
+        public BindingList<string> Products
+        {
+            get { return _products; }
+            set
+            {
+                _products = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+        private BindingList<string> _cart;
+
+        public BindingList<string> Cart
+        {
+            get { return _cart; }
+            set
+            {
+                _cart = value;
+                NotifyOfPropertyChange(() => Cart); 
+            }
+        }
+
+        private string _itemQuantity;
+
+        public string ItemQuantity
+        {
+            get { return _itemQuantity; }
+            set
+            {
+                _itemQuantity = value;
+                NotifyOfPropertyChange(() => ItemQuantity);
+            }
+        }
+
+        public string SubTotal
+        {
+            get 
+            {
+                // TODO - Replace with Calculation
+
+                return "$0.00";
+            } 
+        }
+
+        public string Total
+        {
+            get
+            {
+                // TODO - Replace with Calculation
+
+                return "$0.00";
+            }
+        }
+
+        public string Tax
+        {
+            get
+            {
+                // TODO - Replace with Calculation
+
+                return "$0.00";
+            }
+        }
+
+        public bool CanAddToCart
+        {
+            get
+            {
+                bool output = false;
+
+                // Make sure something is selected
+                // Make sure there is an item quantity
+                if (ItemQuantity?.Length > 0)
+                {
+                    output = true;
+                }
+
+                return output;
+            }
+         
+        }
+
+        public void AddToCart()
+        {
+
+        }
+
+        public bool CanRemoveFromCart
+        {
+            get
+            {
+                bool output = false;
+
+                // Make sure something is selected
+                if (ItemQuantity?.Length > 0)
+                {
+                    output = true;
+                }
+
+                return output;
+            }
+
+        }
+
+        public void RemoveFromCart()
+        {
+
+        }
+
+        public bool CanCheckOut
+        {
+            get
+            {
+                bool output = false;
+
+                // Make sure something is in the Cart
+                if (ItemQuantity?.Length > 0)
+                {
+                    output = true;
+                }
+
+                return output;
+            }
+
+        }
+
+        public void CheckOut()
+        {
+
+        }
+
+
+    }
+}
